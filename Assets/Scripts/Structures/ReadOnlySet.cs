e6c383ec3b51b2e58e37bd73911bbdde99cd810f
AlexeyTipunin <alexeytipunincg@gmail.com>
2023-02-26 05:26:40 +0300

    separate set extension and read only class

--- Assets/Scripts/Structures/ReadOnlySet.cs
+++ Assets/Scripts/Structures/ReadOnlySet.cs
@@ -4,13 +4,6 @@ using System.Collections.Generic;
 
 namespace Assets.Scripts.Structures
 {
-    public static class SetExtensionMethods
-    {
-        public static ReadOnlySet<T> AsReadOnly<T>(this ISet<T> set)
-        {
-            return new ReadOnlySet<T>(set);
-        }
-    }
     public class ReadOnlySet<T> : IReadOnlyCollection<T>
     {
         private readonly ISet<T> _set;
---
+++ Assets/Scripts/Structures/SetExtensionMethods.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Assets.Scripts.Structures
+{
+    public static class SetExtensionMethods
+    {
+        public static ReadOnlySet<T> AsReadOnly<T>(this ISet<T> set)
+        {
+            return new ReadOnlySet<T>(set);
+        }
+    }
+}
